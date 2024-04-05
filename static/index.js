c31eab8dc88db18d679f373f3950cecbbb25928d
atpur-rafif <muhammadatpurrafif@gmail.com>
2024-04-05 19:33:23 +0700

    Search button

--- static/index.js
+++ static/index.js
@@ -1,1 +1,15 @@
-console.log("Hello, worldfjskljf!")
+function $(id) {
+	return document.getElementById(id);
+}
+
+$("search-button").addEventListener("click", async () => {
+	const start = $("input-start").value
+	const end = $("input-end").value
+
+	if (start == "" || end == "") {
+		alert("Input still empty!")
+		return
+	}
+
+	console.log(start, end)
+})

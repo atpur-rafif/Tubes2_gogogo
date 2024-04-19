2e0704501871a3622bef953f8af019d97f4c90cc
atpur-rafif <muhammadatpurrafif@gmail.com>
2024-04-19 20:59:52 +0700

    Refactor getLinks scrapper function

--- links2.go
+++ links2.go
@@ -1,33 +1,38 @@
 package main
 
 import (
-	"log"
 	"net/url"
 	"path/filepath"
 	"strings"
 )
 
-func getLinks2(pages []string, channel chan Links) {
-	for _, page := range pages {
-		url, err := url.JoinPath(WIKI, page)
-		if err != nil {
-			log.Println("Can't join path " + url + " with " + page)
-			return
-		}
+const WIKI = "https://en.wikipedia.org/wiki/"
 
-		resultPages := make([]string, 0)
-		for _, to := range scrap(url) {
-			if strings.HasPrefix(to, WIKI) {
-				toPage, _ := filepath.Rel(WIKI, to)
-				if !strings.ContainsAny(toPage, ":#") {
-					resultPages = append(resultPages, toPage)
+func getPages(links []string) []string {
+	pages := make([]string, 0)
+	for _, to := range links {
+		if strings.HasPrefix(to, WIKI) {
+			toPage, _ := filepath.Rel(WIKI, to)
+			if !strings.ContainsAny(toPage, ":#") {
+				res, err := url.QueryUnescape(toPage)
+				if err != nil {
+					res = toPage
 				}
+				pages = append(pages, res)
 			}
 		}
+	}
+
+	return pages
+}
+
+func getLinks2(pages []string, channel chan Links) {
+	for _, page := range pages {
+		url := WIKI + page
 
 		channel <- Links{
 			From: page,
-			To:   resultPages,
+			To:   getPages(scrap(url)),
 		}
 	}
 }
